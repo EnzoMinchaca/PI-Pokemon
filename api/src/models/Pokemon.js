036deb0addf80923142ab9a1b5e55cbbbceff701
EnzoMinchaca <enzominchak@gmail.com>
2022-08-05 00:58:01 -0300

    Database Finish

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,39 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,   //genera un numero random con letras y numeros unico, datatype ya determinado de sequalize
+      defaultValue: DataTypes.UUIDV4,  //valor por defecto random
+      allowNull: false,  //no permite ser nulo
+      primaryKey: true  //como es id es mi pk
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
     },
+    hp: {
+      type: DataTypes.INTEGER
+    },
+    atack: {
+      type: DataTypes.INTEGER
+    },
+    defense: {
+      type: DataTypes.INTEGER
+    },
+    speed: {
+      type: DataTypes.INTEGER
+    },
+    height: {
+      type: DataTypes.INTEGER
+    },
+    weight: {
+      type: DataTypes.INTEGER
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,    //todos los que cree se van a crear con esta propiedad
+      allowNull: false,
+      defaultValue: true
+    }
   });
 };
