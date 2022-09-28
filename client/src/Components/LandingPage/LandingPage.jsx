a51869e259d88cca77965c154110d4bb2a8ad55d
EnzoMinchaca <enzominchak@gmail.com>
2022-09-28 19:47:24 -0300

    finish

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from '../../Img/pokemonTitle.png'
 import li from '../../Img/link.png'
+import git from '../../Img/github.png'
 import s from './LandingPage.module.css'
 
 export default function LandingPage() {
@@ -15,6 +16,9 @@ export default function LandingPage() {
             <div className={s.content}>
                 <a href="https://www.linkedin.com/in/enzo-minchaca-345809228/" target={"_blank"}>
                     <img className={s.li} src={li} alt="linkedin" />
+                </a>
+                <a href="https://github.com/EnzoMinchaca" target={"_blank"}>
+                    <img className={s.git} src={git} alt="github" />
                 </a>
             </div>
         </div>
